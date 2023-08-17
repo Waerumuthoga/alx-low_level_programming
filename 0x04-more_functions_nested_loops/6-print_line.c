4f065ad34ad1a85be361db7ed295c6552015c546
Waerumuthoga <waerumuthoga@gmail.com>
2023-08-17 06:34:18 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line -draws a line between two points
+ *@n: function parameter
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i;
+
+		for (i = 1 ; i <= n ; i++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
