140754aa1b1ef3caed005bb3f56460621cc5cdbe
Waerumuthoga <waerumuthoga@gmail.com>
2023-08-18 01:30:10 -0700

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -8,25 +8,25 @@
 
 void print_triangle(int size)
 {
+	int x, y, z;
+
 	if (size <= 0)
-		_putchar('\n');
+	{
+	_putchar('\n');
+	}
 	else
 	{
-		int i, j;
-
-		for (i = 1 ; i <= size ; i++)
-		{
-			for (j = 0 ; j < size - i ; j++)
-			{
-				_putchar(' ');
-			}
-
-			for (j = 0 ; j <= i ; j++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
+	for (x = 0; x < size; x++)
+	{
+	for (y = size - x; y > 1; y--)
+	{
+	_putchar(32);
+	}
+	for (z = 0; z <= x; z++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
 	}
 }
-
