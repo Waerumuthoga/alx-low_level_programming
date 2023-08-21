b36d83a6bcc3174a2fae244e74483121675bf520
Waerumuthoga <waerumuthoga@gmail.com>
2023-08-21 14:42:56 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -2,36 +2,28 @@
 
 /**
  * _atoi -converts a string to an integer
- * @s: pointer that will be converted
- * Return: an integer
+ * @s: functional variable
+ * Return: integer
  */
 
 int _atoi(char *s)
 {
-	int c = 0;
-	unsigned int ni = 0;
-	int min = 1;
-	int isi = 0;
+	unsigned int count = 0, size = 0, oi = 0, pn = 1, m = 1, i;
 
-	while (s[c])
+	while (*(s + count) != '\0')
 	{
-		if (s[c] == 45)
-		{
-			min *= -1;
-		}
-
-		while (s[c] >= 48 && s[c] <= 57)
-		{
-			isi = 1;
-			ni = (ni * 10) + (s[c] - '0');
-			c++;
-		}
-		if (isi == 1)
-		{
+		if (size > 0 && (*(s + count) < '0' || *(s + count) > '9'))
 			break;
+
+		if (*(s + count) == '-')
+			pn *= -1;
+
+		if ((*(s + count) >= '0') && (*(s + count) <= '9'))
+		{
+			if (size > 0)
+				m *= 10;
+			size++;
 		}
-		c++;
+		count++;
 	}
-	ni *= min;
-	return (ni);
 }
