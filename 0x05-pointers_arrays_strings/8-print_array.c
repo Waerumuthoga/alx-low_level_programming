0b2d6f593b108112b53e308a827e24c913308d00
Waerumuthoga <waerumuthoga@gmail.com>
2023-08-21 09:30:51 -0700

    task 8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * print_array -prints n elements of an array of integers and a new line
+ * @a: functional variable
+ * @n: functional variable
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int index_of_array;
+
+	for (index_of_array = 0; index_of_array < n ; index_of_array++)
+	{
+		printf("%d", a[index_of_array]);
+		if (index_of_array != (n - 1))
+		{
+			printf(",");
+		}
+	}
+	putchar('\n');
+}
