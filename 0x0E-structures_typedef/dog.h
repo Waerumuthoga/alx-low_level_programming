4361acb54f2b8c9e0fdffb3356cab1eaddace56c
Waerumuthoga <waerumuthoga@gmail.com>
2023-09-11 08:52:54 -0700

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog -a strcut that creates a dog's information
+ * @name: the name of the dog
+ * @age: the age of dog
+ * @owner: the owner of the dog
+ * Description: struct with dog's info
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
