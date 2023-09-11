03baa7422eb8e60fcd6edf30f3af550677d82484
Waerumuthoga <waerumuthoga@gmail.com>
2023-09-11 03:43:20 -0700

    task 4

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,11 @@
+#ifndef SUM_H
+#define SUM_H
+
+/**
+ * File: 4-sum
+ * @x: first int put
+ * @y: second int put
+ * Description: sum up x and y
+ */
+#define SUM(x, y) (x + y)
+#endif /* 4-sum.h */
