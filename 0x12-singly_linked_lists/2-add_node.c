c0d3633f90ec01a8522a4e50d0ee587945d7e864
Waerumuthoga <waerumuthoga@gmail.com>
2023-09-21 15:36:33 -0700

    task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -20,13 +20,13 @@ list_t *add_node(list_t **head, const char *str)
 
 		new_node->next = NULL;
 	else
-	
+
 			new_node->next = *head;
 		new_node->str = strdup(str);
 		new_node->len = len(str);
 		*head = new_node;
 	return (*head);
-	
+
 }
 /**
  * len -get the length of the string
