d30d35f50f2bd79357265549fc95be8a7709504a
Waerumuthoga <waerumuthoga@gmail.com>
2023-09-26 05:39:06 -0700

    task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_listint -frees a linked list
+ * @head: head of the linked list
+ * Return: nothing
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+
+	free(head);
+}
