4aed045f5deb8019368ab84e063882dd0bf91f1f
Waerumuthoga <waerumuthoga@gmail.com>
2023-09-28 13:38:25 -0700

    task 6

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * get_endianness -function that checks the endianness.
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	int y = 1;
+	int little_or_big;
+
+	little_or_big = (int) (((char *) &y)[0]);
+	return (little_or_big);
+}
