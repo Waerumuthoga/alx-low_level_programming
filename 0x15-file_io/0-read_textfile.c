b290303a494fd733a392c2004d17d03e3aadbd70
Waerumuthoga <waerumuthoga@gmail.com>
2023-10-02 10:55:14 -0700

    task 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * read_textfile -reads a text file and prints it to the POSIX standard output.
+ * @filename: the source of the file
+ * @letters: number of letters to read and write
+ * Return: number of letters printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t nrd, nwr;
+	char *buff;
+
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buff = malloc(sizeof(char) * letters);
+	if (!buff)
+		return (0);
+
+	nrd = read(fd, buff, letters);
+	nwr = write(STDOUT_FILENO, buff, nrd);
+
+	close(fd);
+	free(buff);
+
+	return (nwr);
+}
